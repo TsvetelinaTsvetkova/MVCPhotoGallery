fcd8be4089c9b08c7b2b6d23cf076a6a15b450fb
TsvetelinaTsvetkova <cvetelina.cvetkova@yahoo.com>
2016-12-16 12:02:02 +0200

    added album controller and views

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPhotoGallery.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Album
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
